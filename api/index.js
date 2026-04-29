cafc12ad8f2375faa06f1fbaaff1bb14c94f966b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-29 19:50:01 -0300

    feat: Rota de cadastrar produtos

--- api/index.js
+++ api/index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const cors= require("cors");
-const bodyParser=require("body-parser");
+const {v4: uuidv4} = require("uuid");
 
 const app= express();
 const Port = 5001;
 
-app.get("/teste", (req,res)=>{
-     res.send("Seja Bem-vindo ao Teste")
-})
+app.use(cors());
+
+app.use(express.json());
+
+let produtos=[];
+
+// app.get("/teste", (req,res)=>{
+//      res.send("Seja Bem-vindo ao Teste")
+// })
 
 //ROTA PRODUTO- CREATE
-
+app.post("/produtos",(req,res)=>{
+     const {nome,descricao}= req.body;
+     if(!nome || !descricao){
+          return res.status(400).json({error:"Campos Obrigatórios"});
+     }
+     const novoItem ={id:uuidv4(),nome, descricao};
+     produtos.push(novoItem);
+     res.status(201).json(novoItem)
+});
 
 
 
